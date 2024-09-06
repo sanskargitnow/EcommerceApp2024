92a3e92ef2cdde8438633f859a484dc0af6da443
SANSKAR <sasnkar.dixit@yendigital.com>
2024-09-06 15:12:40 +0530

    model done

---
+++ E-Commerce/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using E_Commerce.Data;
+using E_Commerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbcontext _db;
+        public CategoryController(ApplicationDbcontext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+
+        }
+    }
+}
---
+++ E-Commerce/Data/ApplicationDbcontext.cs
@@ -0,0 +1,23 @@
+﻿using E_Commerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Data
+{
+    public class ApplicationDbcontext : DbContext
+    {
+        public ApplicationDbcontext(DbContextOptions<ApplicationDbcontext> options) : base(options)
+        {
+            
+        } 
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           modelBuilder.Entity<Category>().HasData(
+               new Category { Id = 1 , Name = "Mens" , DisplayOrder = 1},
+                 new Category { Id = 2, Name = "Women", DisplayOrder = 2 },
+                  new Category { Id = 3, Name = "Electronics", DisplayOrder = 3 });
+
+        }
+    }
+}
